bde7f1dc3267615318938840c9f4609639fcd8bd
stepagin <dickiysad@gmail.com>
2025-04-26 15:52:17 +0300

    init commit

---
+++ src/main/java/ru/stepagin/dockins/DockinsApplication.java
@@ -0,0 +1,13 @@
+package ru.stepagin.dockins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepagin/dockins/DockinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepagin.dockins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
